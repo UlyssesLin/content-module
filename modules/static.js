0f6040326caaa418a66f111efcbd869218bea9fb
UlyssesLin <utemoc@gmail.com>
2015-01-06 11:30:09 -0800

    second

--- modules/static.js
+++ modules/static.js
@@ -5,14 +5,11 @@ module.exports={
 	find:function(url){
 		if(url=='/'){
 			return('views/index.html');
-		}else if(path.extname(url)=='.css'){
+		}else if(path.extname(url)=='.css' || path.extname(url)=='.jpg'){
 			//substring needed since url auto has "/" in front
-			return(url.substring(1));
-		}else if(url=='/cats.html'){
-			return('views/cats.html');
-		}else if(path.extname(url)=='.jpg'){
-			//substring needed since url auto has "/" in front
-			return(url.substring(1));
+			return url.substring(1);
+		}else if(path.extname(url)=='.html'){
+			return 'views/'+path.basename(url);
 		}else{
 			return null;
 		}
